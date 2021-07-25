66d138b8dc3b989468ace59342ecf98bd7ef25d6
Qingrong Ke <keqingrong1992@gmail.com>
2021-07-25 15:58:05 +0800

    feat: add `toBase64` & `fromBase64`

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,6 +1,35 @@
 import { arrayBufferToString, latin1ToTypedArray } from './blob';
 import { checkMime } from './mime';
 
+export interface Base64Options {
+  /** URL 安全 */
+  urlSafe?: boolean;
+}
+
+/**
+ * 对 Latin1 字符串进行 Base64 编码
+ */
+export function toBase64(data: string, options: Base64Options = {}) {
+  if (options.urlSafe) {
+    return btoa(data)
+      .replace(/\+/g, '-')
+      .replace(/\//, '_');
+  } else {
+    return btoa(data);
+  }
+}
+
+/**
+ * 对 Base64 数据进行解码
+ */
+export function fromBase64(data: string, options: Base64Options = {}) {
+  if (options.urlSafe) {
+    return atob(data.replace(/\-/g, '+').replace(/_/g, '/'));
+  } else {
+    return atob(data);
+  }
+}
+
 /**
  * 将 ArrayBuffer 对象转换成 base64 字符串
  */
